045bf4ae7df155e256f42465aa0a0015778a3045
nazrulrachmadiae <142975399+nazrulrachmadiae@users.noreply.github.com>
2023-08-23 15:33:15 +0700

    Update class_bot_wa.py

--- class_bot_wa.py
+++ class_bot_wa.py
@@ -1,3 +1,3 @@
 _UNREAD_='l7jjieqr.cfzgl7ar.ei5e7seu.h0viaqh7.tpmajp1w.c0uhu3dl.riy2oczp.dsh4tgtl.sy6s5v3r.gz7w46tb.lyutrhe2.qfejxiq4.fewfhwl7.ovhn1urg.ap18qm3b.ikwl5qvt.j90th5db.aumms1qt'
-_NAMAKONTAK_='_21S-L'
+_NAMAKONTAK_='ggj6brxn.gfz4du6o.r7fjleex.g0rxnol2.lhj4utae.le5p0ye3.l7jjieqr._11JPr'
 _TEXTBOX_='p3_M1'
